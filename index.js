8ef9f5ae4f3298c7985e1907d4f1c7f25195ad89
loganyeh <yehlogan@gmail.com>
2025-10-29 17:10:59 -0400

    capitaled city name

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ const searchButton = document.getElementById("search-button");
 
 // EVENT LISTENERS
 searchButton.addEventListener("click", () => {
-    const cityName = inputBar.value;
-    console.log(`The city is ${cityName}`);
-    
-    clearInputBar();
-});
 
+    const cityName = inputBar.value;
+    const cleanedCityName = cityName.trimStart();
+    const capitalizedCityName = capitalizeFirstLetter(cleanedCityName);
+
+
+    if(inputBar.value == ""){
+        // window.alert(`Enter a City.`);
+        console.log("Enter a city");
+    } 
+    // else if () MAYBE ADD THING FOR IF THERE IS A NUMBER
+    else {
+        //  CHECK IF CITY IS VALID 
+        
+
+        console.log(`${capitalizedCityName}`);
+    }
+
+    getCityName(capitalizedCityName);
+    clearInputBar();
+
+});
 
 // CALLS
 
@@ -23,15 +39,32 @@ function clearInputBar(){
     inputBar.value = "";
 };
 
+function capitalizeFirstLetter(word){
+    const lowercasedString = word.toLowerCase();
+    return lowercasedString.charAt(0).toUpperCase() + lowercasedString.slice(1);
+}
 
 // ASYNCS
+    // GEOCODING API URL
+async function getCityName(city){
+
+    try {
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`);
+        const data = await response.json();
+    
+        console.log("-----");
+        console.log(data);
+    }
+    catch(error){
+        console.error(`Error in getCityName(): ${error}`);
+    }
+
+}
+
 
 
 // SANDBOX
-console.log("JS WORKING");
-console.log("------------------");
-
-
+console.log(".....JS WORKING")
 
 
 
