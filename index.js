f63a7dbd73c57c2aad937dc3cb6b551c8cc79001
loganyeh <yehlogan@gmail.com>
2025-10-29 16:32:05 -0400

    added event listener for search

--- index.js
+++ index.js
@@ -1,51 +1,38 @@
 
-// DOMS VARIABLES
+// DOMS VARIABLES 
+const inputBar = document.getElementById("input-bar");
+const searchButton = document.getElementById("search-button");
 
-
-// VARIABLES
-
-
-
-
+// VARIABLES 
+// const cityName = "";
 
 
 // EVENT LISTENERS
+searchButton.addEventListener("click", () => {
+    const cityName = inputBar.value;
+    console.log(`The city is ${cityName}`);
+    
+    clearInputBar();
+});
+
+
+// CALLS
+
+// FUNCTIONS
+function clearInputBar(){
+    inputBar.value = "";
+};
+
+
+// ASYNCS
+
+
+// SANDBOX
+console.log("JS WORKING");
+console.log("------------------");
 
 
 
-
-
-
-// API CALL
-async function getWeather(){
-    try{
-        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code,relative_humidity_2m,apparent_temperature,precipitation_probability,precipitation,wind_speed_10m&current=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,precipitation,weather_code");
-        const data = await response.json();
-        console.log("-----getWeather API Call-----");
-        console.log(data);  
-    }
-    catch(error){
-        console.error(`Error: ${error}`);
-    }
-
-}
-
-async function getName(location){
-    try{
-        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=10&language=en&format=json`)
-        const data = await response.json();
-        console.log("-----getName() function-----")
-        console.log(data);
-    }
-    catch(error){
-        console.error(`Error: ${error}`);
-    }
-
-}
-
-console.log("----- WORKING JS FILE -----");
-// getWeather();
-// getName();
 
 
 
